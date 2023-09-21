4161e023c16cdfa0e102792bc96b26c2b9470e37
Del1riumpl <octoboiacc@gmail.com>
2023-09-21 12:49:51 +0300

    Добавьте файлы проекта.

---
+++ TaipanKernel/Kernel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace TaipanKernel
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+        }
+
+        protected override void Run()
+        {
+            Console.Write("Input: ");
+            var input = Console.ReadLine();
+        }
+    }
+}
